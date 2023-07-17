f8aaeb508f8f0fd47743fbdc22482801890b1af3
profinvincible <muoghaluijeomaijeoma9@gmail.com>
2023-07-17 05:58:33 +0100

    new

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3030;
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
