a1444c833f47e1f6aa26bb0cd4d3b8c024c1ad1d
profinvincible <muoghaluijeomaijeoma9@gmail.com>
2023-07-30 22:40:04 +0100

    m

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql");
 
-const db = mysql.createPool({
-  user: "admin",
-  host: "mysql-136293-0.cloudclusters.net",
-  password: "j2dABS8v",
-  database: "my_blog",
-  IPAddress: "181.215.242.87",
-  port: "10035",
-});
+// const db = mysql.createPool({
+//   user: "admin",
+//   host: "mysql-136293-0.cloudclusters.net",
+//   password: "j2dABS8v",
+//   database: "my_blog",
+//   IPAddress: "181.215.242.87",
+//   port: "10035",
+// });
 
 // const db = mysql.createPool({
 //   user: "root",
@@ -15,5 +15,13 @@ const db = mysql.createPool({
 //   password: "myanchorholds12#",
 //   database: "my_blog",
 // });
+
+const db = mysql.createPool({
+  user: "4352169_blog",
+  host: "fdb1028.awardspace.net",
+  password: "+b9}mcjo3W:HV)Pq",
+  database: "	my_blog",
+  port: "3306",
+});
 
 module.exports = db;
