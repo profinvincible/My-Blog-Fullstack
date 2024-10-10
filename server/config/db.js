a59af27d0b5bc04c34a2bc04abfb7b7285bfa570
ProfInvincible <muoghaluijeoma9@gmail.com>
2024-10-10 23:46:53 +0100

    latest backend

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,22 @@
 const mysql2 = require("mysql2");
 
-// const db = mysql.createPool({
-//   user: "admin",
-//   host: "mysql-136293-0.cloudclusters.net",
-//   password: "j2dABS8v",
-//   database: "my_blog",
-//   IPAddress: "181.215.242.87",
-//   port: "10035",
-// });
 
-// const db = mysql.createPool({
-//   user: "root",
-//   host: "localhost",
-//   password: "myanchorholds12#",
-//   database: "my_blog",
-// });
 
 const db = mysql2.createPool({
   user: "root",
-  host: "containers-us-west-209.railway.app",
-  password: "cEZ6wbxDpDKsQQb2RZz3",
-  database: "railway",
-  port: "7856",
-  url: "mysql://root:cEZ6wbxDpDKsQQb2RZz3@containers-us-west-209.railway.app:7856/railway",
+  host: "localhost",
+  password: "profinvincible20$",
+  database: "mini_blog",
 });
+
+
+// const db = mysql2.createPool({
+//   user: "root",
+//   host: "mysql-108709f8-muoghaluijeoma9-916f.j.aivencloud.com",
+//   password: "AVNS_bvYEHhTDMOs9d-Hm0rx",
+//   database: "defaultdb",
+//   port: "24205",
+//   url: "mysql://avnadmin:AVNS_bvYEHhTDMOs9d-Hm0rx@mysql-108709f8-muoghaluijeoma9-916f.j.aivencloud.com:24205/defaultdb?ssl-mode=REQUIRED",
+// });
 
 module.exports = db;
